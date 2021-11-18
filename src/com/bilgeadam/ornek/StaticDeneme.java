73e52db15557f45ab6ca1c7eb90d226b26aa7248
Irem Gunay <iremguunay@gmail.com>
2021-11-18 23:53:09 +0300

    Static Deneme örneği eklendi

--- src/com/bilgeadam/ornek/StaticDeneme.java
+++ src/com/bilgeadam/ornek/StaticDeneme.java
@@ -5,7 +5,23 @@ public class StaticDeneme {
     // Instance variable
     int sayi1;
 
+    // Class veriable - değişken static ise class variable'dır
+    static int sayi2;
+
     public static void main(String[] args) {
+
+        //static olmayan değişkene static method'dan erişilemez.
+        //erişmek istiyorsak, class'ın kendi objesini oluşturup erişeceğiz.
+
+        // Local variable, initialize etmek zorundayız
+        // Yani ilk değeri vermek zorundayız
+        int sayi3 = 0;
+
+        StaticDeneme sd = new StaticDeneme();
+        System.out.println("Sayı1 = " + sd.sayi1);
+
+        System.out.println("Sayı2 = " + sayi2);
+        System.out.println("Sayı3 = " + sayi3);
 
     }
 }
